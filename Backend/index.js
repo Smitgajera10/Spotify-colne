1759dfb1652334d41d5ca1b47a7f58b5d94f424f
Smitgajera10 <101303441+Smitgajera10@users.noreply.github.com>
2025-06-09 07:14:07 +0000

    for deployment security

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const JwtStrategy = require("passport-jwt").Strategy,
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // for security of connecting fronted to backend ip
 app.use(cors());
--- Backend/utils/helpers.js
+++ Backend/utils/helpers.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 exports = {};
 
 exports.getToken = async (email, user) => {
-  const token = jwt.sign({ identifier: user._id }, process.env.JWT_SECRET || "secret", {
+  const token = jwt.sign({ identifier: user._id }, process.env.JWT_SECRET, {
     expiresIn: "15d",});
   return token;
 };
