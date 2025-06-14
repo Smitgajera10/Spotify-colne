385ad0f4da3eb934a72e209213c6b122e3be20a6
Smitgajera10 <101303441+Smitgajera10@users.noreply.github.com>
2025-06-14 14:44:26 +0530

    Update SingleSongCard.jsx

--- Frontend/src/components/SingleSongCard.jsx
+++ Frontend/src/components/SingleSongCard.jsx
@@ -16,7 +16,7 @@ const SingleSongCard = ({info , playSound })=>{
             <div className="flex w-full"> 
                 <div className="text-white flex justify-center flex-col pl-4 w-5/6">
                     <div className="cursor-pointer hover:underline">{info.name}</div>
-                    <div className="text-xs text-gray-400 cursor-pointer hover:underline"> {info.artist.firstName + " " + info.artist.lastName} </div>
+                    <div className="text-xs text-gray-400 cursor-pointer hover:underline"> {info.artist?.firstName + " " + info.artist?.lastName} </div>
                 </div>
 
                 <div className="w-1/6 flex items-center justify-center text-gray-400 text-sm">
